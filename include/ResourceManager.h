ceea17b36d7e64888692e2cb65bee5fc357acb65
Kyle <abramowitzk@gmail.com>
2017-01-04 17:30:42 -0500

    Fixed some compiler errors

--- include/ResourceManager.h
+++ include/ResourceManager.h
@@ -1,30 +1,31 @@
 #pragma once
 #include <string>
-#include <Mesh.h>
 #include <tinyxml2.h>
 #include <vector>
 #include <unordered_map>
-#include <Texture.h>
-using namespace std;
-class ResourceManager {
-public:
-	ResourceManager();
-	~ResourceManager();
-	void LoadScene(const std::string& name);
-	Texture* GetTexture(string name) {
-		return &m_textures[name];
-	}
+#include <texture.hpp>
+namespace CBlocks {
+	using namespace std;
+	class ResourceManager {
+	public:
+		ResourceManager();
+		~ResourceManager();
+		void LoadScene(const std::string& name);
+		Texture* GetTexture(string name) {
+			return &m_textures[name];
+		}
 
-private:
-	void LoadTexture(string name);
-	void LoadModel(const char* name);
-	const std::string ResourcePath = "..\\..\\Data\\";
-	const std::string TexturePath = ResourcePath + "Textures\\";
-	const std::string ModelPath = ResourcePath + "Models\\";
-	const std::string FontPath = ResourcePath + "Fonts\\";
-	const std::string AudioPath = ResourcePath + "Sounds\\";
-	const std::string ScenePath = ResourcePath + "Scenes\\";
+	private:
+		void LoadTexture(string name);
+		void LoadModel(const char* name);
+		const std::string ResourcePath = "..\\..\\Data\\";
+		const std::string TexturePath = ResourcePath + "Textures\\";
+		const std::string ModelPath = ResourcePath + "Models\\";
+		const std::string FontPath = ResourcePath + "Fonts\\";
+		const std::string AudioPath = ResourcePath + "Sounds\\";
+		const std::string ScenePath = ResourcePath + "Scenes\\";
 
-	unordered_map<string, Texture> m_textures;
-};
+		unordered_map<string, Texture> m_textures;
+	};
 
+}
